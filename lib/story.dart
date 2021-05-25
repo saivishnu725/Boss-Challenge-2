644647378b27bacdae73f920d6a9c4498f7c652e
saivishnu725 <saivishnu725@gmail.com>
2021-05-25 21:41:34 +0530

    TODO 7

--- lib/story.dart
+++ lib/story.dart
@@ -3,7 +3,7 @@ class Story {
   String choice1; // Store the text for choice 1.
   String choice2; // Store the text for choice 2.
 
-  Story(String storyTitle, String choice1, String choice2) {
+  Story({String storyTitle, String choice1, String choice2}) {
     this.storyTitle = storyTitle;
     this.choice1 = choice1;
     this.choice2 = choice2;
