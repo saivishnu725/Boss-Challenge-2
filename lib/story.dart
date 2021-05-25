bebb3af20c7721e2c55c7938ea7706d466f9d596
saivishnu725 <saivishnu725@gmail.com>
2021-05-25 21:38:59 +0530

    TODO 4

--- lib/story.dart
+++ lib/story.dart
@@ -1,7 +1,11 @@
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
-
 class Story {
   String storyTitle; // Store the story text.
   String choice1; // Store the text for choice 1.
   String choice2; // Store the text for choice 2.
+
+  Story(String storyTitle, String choice1, String choice2) {
+    this.storyTitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
+  }
 }
