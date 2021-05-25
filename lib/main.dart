a42323ca8adb3960af8e370c7bd18dcbb910d0a7
saivishnu725 <saivishnu725@gmail.com>
2021-05-25 21:46:10 +0530

    TODO 10

--- lib/main.dart
+++ lib/main.dart
@@ -41,8 +41,7 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    'Story text will go here.',
+                    storyBrain.getStory(),
                     style: TextStyle(
                       fontSize: 25.0,
                     ),
